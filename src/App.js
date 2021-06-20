7ceae4b0b45a46f564ffb575183fa868ef5ba742
Dimas014 <dimas05014@gmail.com>
2021-06-20 16:37:32 +0700

    Fourth Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       </header>
       <body>
-        Body
+        <p className="code">Body</p>
 
       </body>
       <footer>
